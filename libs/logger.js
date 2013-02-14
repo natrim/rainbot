cf077660d07b59f7c6efc802f8b87e18f9e39c88
Natrim <maly@kinet.cz>
2013-02-14 15:48:16 +0100

    logger

---
+++ libs/logger.js
@@ -0,0 +1,49 @@
+function Logger() {
+  this.enabled = true;
+}
+
+Logger.prototype.onBeforeLog = function() {};
+Logger.prototype.onAfterLog = function() {};
+
+Logger.prototype.log = function(msg, level) {
+  if(!this.enabled) {
+    return;
+  }
+
+  var dontlog = this.onBeforeLog(arguments) || false;
+  if(!dontlog) {
+    switch(level) {
+    case 'error':
+      console.error(msg);
+      break;
+    case 'warn':
+      console.warn(msg);
+      break;
+    case 'info':
+      console.info(msg);
+      break;
+    case 'log':
+      console.log(msg);
+      break;
+    default:
+      console.log(msg);
+    }
+  }
+  this.onAfterLog(dontlog || false, arguments);
+};
+
+Logger.prototype.error = function(msg) {
+  this.log(msg, 'error');
+};
+
+Logger.prototype.warn = function(msg) {
+  this.log(msg, 'warn');
+};
+
+Logger.prototype.info = function(msg) {
+  this.log(msg, 'info');
+};
+
+
+module.exports = new Logger();
+module.exports.Logger = Logger;
