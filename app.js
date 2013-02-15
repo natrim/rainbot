f0a36711cfe98f7cf4ab2176cc5d47b41d0ce443
Natrim <maly@kinet.cz>
2013-02-15 14:45:07 +0100

    remove the unneeded deps will add them alter as i use them

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-//set some shortcuts
-//global._ = require("underscore");
-//global.a = require("async");
-//global.dateFormat = require("./libs/helpers").dateFormat;
-var bot = new(require('./libs/bot').Bot)(); //create bot
+//create bot
+var bot = new(require('./libs/bot').Bot)();
 // OR var bot = require('./libs/bot').create();
 bot.loadConfig(); //load the config from file - default is config.json
 bot.loadModules(); //preload all active modules - default is from config or modules.json
