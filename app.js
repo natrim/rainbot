d9f0343cc2647cc724f1c5d641a3327a95400356
Natrim <maly@kinet.cz>
2013-03-13 11:05:00 +0100

    use domain to catch unmatched errors in app

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 //create bot
-var bot = new(require('./libs/bot').Bot)();
-// OR var bot = require('./libs/bot').create();
-bot.loadConfig(); //load the config from file - default is config.json
-bot.loadModules(); //preload all active modules - default is from config or modules.json
-bot.run(); //and run the bot
+
+var botDomain = require('domain').create();
+var logger = require('./libs/logger');
+
+botDomain.on('error', function(e) {
+	logger.error(e);
+});
+
+botDomain.run(function() {
+	var bot = new(require('./libs/bot').Bot)();
+	// OR var bot = require('./libs/bot').create();
+	bot.loadConfig(); //load the config from file - default is config.json
+	bot.loadModules(); //preload all active modules - default is from config or modules.json
+	bot.run(); //and run the bot
+});
