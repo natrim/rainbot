a9d906511d267328b61e12dd5658cccbddef5d20
Natrim <maly@kinet.cz>
2013-04-03 11:55:17 +0200

    irclog module

---
+++ modules/irclog.js
@@ -0,0 +1,36 @@
+/**
+ * IRC file log
+ */
+
+/* jslint node: true */
+/* global BOT_DIR, LIBS_DIR, MODULES_DIR */
+'use strict';
+
+var fs = require('fs');
+
+exports.init = function(reload) {
+	if (typeof this.config.logfile === 'undefined' || this.config.logfile === '') {
+		return;
+	}
+
+	if (!reload || !this.logfile) {
+		this.logfile = fs.createWriteStream(this.config.logfile, {
+			flags: 'a',
+			encoding: 'utf-8'
+		});
+	}
+
+	var m = this;
+	this.dispatcher.on('irc/RECV', function(msg) {
+		m.logfile.write(Math.floor(Date.now() / 1000) + ' ' + msg + '\n');
+	});
+	this.dispatcher.on('irc/SEND', function(msg) {
+		m.logfile.write(Math.floor(Date.now() / 1000) + ' ' + msg + '\n');
+	});
+};
+
+exports.halt = function(reload) {
+	if (!reload && this.logfile) {
+		this.logfile.end();
+	}
+};
