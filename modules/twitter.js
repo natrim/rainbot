62d6b43f643b41438726b9e0edbe0b527c28c1d7
Natrim <maly@kinet.cz>
2013-04-04 11:30:29 +0200

    twitter module refactoring

--- modules/twitter.js
+++ modules/twitter.js
@@ -9,7 +9,14 @@
 var https = require('https');
 var df = require(LIBS_DIR + '/helpers').dateFormat;
 
-function getTweet(source, user, number, wait, dateFormat) {
+function Twitter(config) {
+    this.config = config;
+}
+
+Twitter.prototype.getTweet = function(source, user, number) {
+    var wait = this.config.waitBetweenMessages;
+    var dateFormat = this.config.dateFormat;
+
     https.get({
         host: 'api.twitter.com',
         path: '/1/statuses/user_timeline.json?include_rts=1&screen_name=' + user + '&count=' + number
@@ -58,57 +65,57 @@ function getTweet(source, user, number, wait, dateFormat) {
     }).on('error', function(e) {
         source.mention('i got error: ' + e.message);
     });
-}
+};
+
+Twitter.prototype.respond = function(source, argv) {
+    var user = this.config.defaultUser;
+    var number = 1;
+
+    if (argv[0]) {
+        if (/^[0-9]+$/.test(argv[0])) {
+            number = argv[0];
+        } else if (/[a-zA-Z0-9_]{1,15}/.test(argv[0])) {
+            user = argv[0];
+        }
+    }
+
+    if (argv[1]) {
+        if (/^[0-9]+$/.test(argv[1])) {
+            number = argv[1];
+        }
+    }
+
+    if (number > this.config.maxTweets) {
+        source.mention('i can catch only last ' + this.config.maxTweets + ' birds at once!');
+        number = this.config.maxTweets;
+    }
+
+    source.action('is catching' + (number > 1 ? ' ' + number : '') + ' blue bird' + (number > 1 ? 's' : '') + ' with message from \'' + user + '\'!');
+    this.getTweet(source, user, number);
+};
 
 exports.init = function() {
-    var config = this.config;
-
-    if (typeof config.maxTweets === 'undefined') {
-        config.maxTweets = 5;
+    if (typeof this.config.maxTweets === 'undefined') {
+        this.config.maxTweets = 5;
     }
 
-    if (typeof config.waitBetweenMessages === 'undefined') {
-        config.waitBetweenMessages = 1000;
+    if (typeof this.config.waitBetweenMessages === 'undefined') {
+        this.config.waitBetweenMessages = 1000;
     }
 
-    if (typeof config.defaultUser === 'undefined') {
-        config.defaultUser = 'MyLittlePony';
+    if (typeof this.config.defaultUser === 'undefined') {
+        this.config.defaultUser = 'MyLittlePony';
     }
 
-    if (typeof config.dateFormat === 'undefined') {
-        config.dateFormat = 'DD.MM.YYYY HH:II:SS';
+    if (typeof this.config.dateFormat === 'undefined') {
+        this.config.dateFormat = 'DD.MM.YYYY HH:II:SS';
     }
 
-    function twitterCommand(source, argv) {
-        var user = config.defaultUser;
-        var number = 1;
-
-        if (argv[0]) {
-            if (/^[0-9]+$/.test(argv[0])) {
-                number = argv[0];
-            } else if (/[a-zA-Z0-9_]{1,15}/.test(argv[0])) {
-                user = argv[0];
-            }
-        }
-
-        if (argv[1]) {
-            if (/^[0-9]+$/.test(argv[1])) {
-                number = argv[1];
-            }
-        }
-
-        if (number > config.maxTweets) {
-            source.mention('i can catch only last ' + config.maxTweets + ' birds at once!');
-            number = config.maxTweets;
-        }
-
-        source.action('is catching' + (number > 1 ? ' ' + number : '') + ' blue bird' + (number > 1 ? 's' : '') + ' with message from \'' + user + '\'!');
-        getTweet(source, user, number, config.waitBetweenMessages, config.dateFormat);
-    }
+    this.twitter = new Twitter(this.config);
 
     var c = this.require('controls');
-    c.addCommand('tweet', twitterCommand);
-    c.addCommand('twitter', twitterCommand);
+    c.addCommand('tweet', this.twitter.respond.bind(this.twitter));
+    c.addCommand('twitter', this.twitter.respond.bind(this.twitter));
 };
 
 exports.halt = function(bot) {
