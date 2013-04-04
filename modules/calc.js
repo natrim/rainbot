29196da05767051cfff46e4a98209d143cbd3aea
Natrim <maly@kinet.cz>
2013-04-04 11:21:29 +0200

    make the calc module work without curl

--- modules/calc.js
+++ modules/calc.js
@@ -6,35 +6,45 @@
 /* global BOT_DIR, LIBS_DIR, MODULES_DIR */
 
 /* ------------------------------ Includes && Options ----------------- */
-var exec = require('child_process').exec;
+var request = require('request');
 
 /* ------------------------------ WolframAlpha ------------------------ */
 
 function WolframAlpha() {
-	this.search = function(query, hollaback) {
-		var result = {
-			url: 'http://www.wolframalpha.com/input/?i=' + encodeURIComponent(query)
-		};
-
-		exec('curl -e \'http://www.wolframalpha.com\' \'' + result.url + '\'', function(err, stdout, stderr) {
-			var solution = />Solution:<[\s\S]*?alt\s*=\s*\"([^\""]*)\"/,
-				other = /stringified"\s*:\s*"([^"\r\n]*)/g;
-
-			if (solution.test(stdout)) {
-				result.data = stdout.match(solution)[1].replace(/\\\//, '/');
-			} else {
-				var match = stdout.match(other);
-				if (!match || !match[1]) {
-					result.data = null;
-				} else {
-					result.data = match[1].replace(/stringified"\s*:\s*"/g, '').replace(/\\n/g, ' ').replace(/\\\//, '/');
-				}
-			}
-
-			hollaback.call(this, result);
-		});
-	};
+	this.solution = />Solution:<[\s\S]*?alt\s*=\s*\"([^\""]*)\"/;
+	this.other = /stringified"\s*:\s*"([^"\r\n]*)/g;
+	this.get_other = /stringified"\s*:\s*"/g;
 }
+
+WolframAlpha.prototype.search = function(query, callback) {
+	var result = {
+		url: 'http://www.wolframalpha.com/input/?i=' + encodeURIComponent(query)
+	};
+	var WA = this;
+	request(result.url, function(error, response, body) {
+		if (error || response.statusCode !== 200) {
+			callback.call(this, result);
+			return;
+		}
+
+		var match = body.match(WA.solution);
+
+		if (match) {
+			result.data = match[1].replace(/\\\//, '/');
+		} else {
+			match = body.match(WA.other);
+			if (!match || !match[1]) {
+				result.data = null;
+			} else {
+				result.data = match[1].replace(WA.get_other, '').replace(/\\n/g, ' ').replace(/\\\//, '/');
+			}
+		}
+
+		callback.call(this, result);
+	});
+};
+
+/* ------------------------------ module ---------------------------- */
 
 exports.init = function(bot, dispatcher, calc) {
 	var wa = new WolframAlpha();
@@ -48,7 +58,7 @@ exports.init = function(bot, dispatcher, calc) {
 		source.action('casts the magic of math and ...');
 
 		wa.search(argv.join(' '), function(result) {
-			source.mention(result.data ? 'the answer is: ' + result.data.replace(/\\'/g, '\'') : 'i don\'t know the answer :(');
+			source.mention(result.data ? 'the answer to your equation is: ' + result.data.replace(/\\'/g, '\'') : 'i don\'t know the answer...');
 		});
 	});
 };
