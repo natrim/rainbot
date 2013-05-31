22e9be7aad28df0f3d6dfbb29e3704ab64c9b7af
Natrim <maly@kinet.cz>
2013-05-31 11:43:58 +0200

    use chai assert

--- test/config-test.js
+++ test/config-test.js
@@ -16,41 +16,13 @@ global.MODULES_DIR = require('path').resolve(BOT_DIR, 'modules');
 require(LIBS_DIR + '/logger').enabled = false;
 
 //load assert lib
-var assert = require('assert');
+var assert = require('chai').assert;
 
-//assert helpers
-assert.isError = function(val) {
-	assert.isObject(val);
-	assert.instanceOf(val, Error);
-};
-
-assert.isObject = function(o) {
-	return typeof o === 'object';
-};
-
-assert.instanceOf = function(real, expected) {
-	return real instanceof expected;
-};
-
-assert.isTrue = function(v) {
-	return v === true;
-};
-
-assert.isFalse = function(v) {
-	return v === false;
-};
-
-assert.isUndefined = function(v) {
-	return typeof v === 'undefined';
-};
-
-//and letz go
 var Config = require(LIBS_DIR + '/config');
 
 describe('Config lib', function() {
 	describe('class construction', function() {
 		it('should return \'Config\' instance', function() {
-			assert.isObject(Config);
 			assert.instanceOf(new Config.Config(), Config.Config);
 			assert.instanceOf(Config.create(), Config.Config);
 		});
