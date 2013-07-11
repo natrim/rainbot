366936c3809aeb5f4c9c0da89a78fcf38047e777
Natrim <maly@kinet.cz>
2013-07-11 15:59:08 +0200

    add formatSizeUnits helper

--- libs/helpers.js
+++ libs/helpers.js
@@ -73,6 +73,28 @@ module.exports.dateFormat = module.exports.formatDate = module.exports.formatted
 };
 
 /**
+ * Formats size in bytes to pretty string
+ * @param  {number} bytes
+ * @return {string}
+ */
+module.exports.formatSizeUnits = function formatSizeUnits(bytes) {
+	if (bytes >= 1073741824) {
+		bytes = (bytes / 1073741824).toFixed(2) + ' GB';
+	} else if (bytes >= 1048576) {
+		bytes = (bytes / 1048576).toFixed(2) + ' MB';
+	} else if (bytes >= 1024) {
+		bytes = (bytes / 1024).toFixed(2) + ' KB';
+	} else if (bytes > 1) {
+		bytes = bytes + ' bytes';
+	} else if (bytes >= 1) {
+		bytes = bytes + ' byte';
+	} else {
+		bytes = '0 bytes';
+	}
+	return bytes;
+};
+
+/**
  * Wraps function in another function
  * @param  {function} func
  * @param  {function} wrapper
