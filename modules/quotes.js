a6847a5f80843963f0cef572fca2614f12bc7c46
Natrim <maly@kinet.cz>
2013-12-16 09:50:45 +0100

    simple “choose a pony” in quotes aka Ponydex

--- modules/quotes.js
+++ modules/quotes.js
@@ -5,6 +5,7 @@
 'use strict';
 
 var quotes;
+var ponydex = {};
 var lastQuote = '';
 
 function loadQuotes() {
@@ -16,26 +17,42 @@ function loadQuotes() {
 			return;
 		}
 		forEachAsync(list, function (file) {
-			var ext = file.split('.').pop();
+			var tmp = file.split('.');
+			var ext = tmp.pop();
+			var pony = tmp.join('.');
 			if (ext === 'json') {
 				var data = require(MODULES_DIR + '/quotes/' + file);
 				if (typeof data === 'object' && data instanceof Array) {
 					quotes = quotes.concat(data);
+					ponydex[pony] = quotes.slice(quotes.length - data.length, quotes.length);
 				}
 			}
 		});
 	});
 }
 
-function getRandomQuote() {
+function getRandomQuote(pony) {
+	pony = slug(pony) || null;
 	if (!(quotes instanceof Array) || quotes.length <= 0) {
 		return 'Quotes have been not loaded!';
 	}
+
+	if (pony && typeof ponydex[pony] !== 'undefined') {
+		return ponydex[pony][Math.floor(Math.random() * ponydex[pony].length)];
+	}
+
 	return quotes[Math.floor(Math.random() * quotes.length)];
 }
 
-function quote(source) {
-	var q = getRandomQuote();
+function quote(source, args) {
+	var pony = null;
+	if (typeof args.input !== 'undefined') {
+		pony = args[2];
+	} else {
+		pony = args.join(' ');
+	}
+
+	var q = getRandomQuote(pony);
 	if (q === lastQuote) {
 		q = getRandomQuote();
 	}
@@ -43,10 +60,18 @@ function quote(source) {
 	lastQuote = q;
 }
 
+function slug(word) {
+	if (typeof word !== 'string') {
+		return false;
+	}
+
+	return word.toLowerCase().replace(/[^a-z0-9_]+/g, '');
+}
+
 exports.init = function () {
 
 	loadQuotes();
 
 	this.addCommand('quote', quote).addCommand('quotes', quote).addCommand('q', quote);
-	this.addAction('quote', quote, /^(quotes|quote|q)$/i, false);
+	this.addAction('quote', quote, /^(quotes|quote|q)[ ]?(.*)$/i, false);
 };
