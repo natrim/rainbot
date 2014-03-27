c9c6a0c7feb2529570b64e42f7a5e3d2fac645c5
Natrim <maly@kinet.cz>
2014-03-27 08:32:50 +0100

    last seen move file

--- modules/lastseen.js
+++ modules/lastseen.js
@@ -3,12 +3,12 @@
 var time = require('time'), logger = require(LIBS_DIR + '/logger');
 
 //main data storage
-var lasthash = {};
+var lastSeenHashStorage = {};
 
 /**
  Calculates the difference between two unix times and returns
  a string like '15d 23h 42m 15s ago'
-*/
+ */
 function calcDiff(when) {
 	var now = (new time.Date().getTime() / 1000);
 	var diff = (now - when);
@@ -31,11 +31,11 @@ function calcDiff(when) {
 //hook for nick changes
 function onNick(source, args) {
 	if (args[0]) {
-		lasthash[source.nick.toLowerCase()] = {
+		lastSeenHashStorage[source.nick.toLowerCase()] = {
 			'last': (new time.Date().getTime() / 1000),
 			'words': '"' + source.nick + '" changed nick to "' + args[0] + '"'
 		};
-		lasthash[args[0].toLowerCase()] = {
+		lastSeenHashStorage[args[0].toLowerCase()] = {
 			'last': (new time.Date().getTime() / 1000),
 			'words': '"' + args[0] + '" changed nick from "' + source.nick + '"'
 		};
@@ -45,12 +45,12 @@ function onNick(source, args) {
 //hook for people quitting
 function onQuit(source, args) {
 	if (args[0]) {
-		lasthash[source.nick.toLowerCase()] = {
+		lastSeenHashStorage[source.nick.toLowerCase()] = {
 			'last': (new time.Date().getTime() / 1000),
 			'words': '"' + source.nick + '" quit IRC stating "' + args[0] + '"'
 		};
 	} else {
-		lasthash[source.nick.toLowerCase()] = {
+		lastSeenHashStorage[source.nick.toLowerCase()] = {
 			'last': (new time.Date().getTime() / 1000),
 			'words': '"' + source.nick + '" quit IRC with no reason'
 		};
@@ -60,12 +60,12 @@ function onQuit(source, args) {
 //hook for people joining
 function onJoin(source, args) {
 	if (args[0]) {
-		lasthash[source.nick.toLowerCase()] = {
+		lastSeenHashStorage[source.nick.toLowerCase()] = {
 			'last': (new time.Date().getTime() / 1000),
 			'words': '"' + source.nick + '" joined channel "' + args[0] + '"'
 		};
 	} else {
-		lasthash[source.nick.toLowerCase()] = {
+		lastSeenHashStorage[source.nick.toLowerCase()] = {
 			'last': (new time.Date().getTime() / 1000),
 			'words': '"' + source.nick + '" joined'
 		};
@@ -76,18 +76,18 @@ function onJoin(source, args) {
 function onPart(source, args) {
 	if (args[0]) {
 		if (args[1]) {
-			lasthash[source.nick.toLowerCase()] = {
+			lastSeenHashStorage[source.nick.toLowerCase()] = {
 				'last': (new time.Date().getTime() / 1000),
 				'words': '"' + source.nick + '" left channel "' + args[0] + '" stating "' + args[1] + '"'
 			};
 		} else {
-			lasthash[source.nick.toLowerCase()] = {
+			lastSeenHashStorage[source.nick.toLowerCase()] = {
 				'last': (new time.Date().getTime() / 1000),
 				'words': '"' + source.nick + '" left channel "' + args[0] + '"'
 			};
 		}
 	} else {
-		lasthash[source.nick.toLowerCase()] = {
+		lastSeenHashStorage[source.nick.toLowerCase()] = {
 			'last': (new time.Date().getTime() / 1000),
 			'words': '"' + source.nick + '" left'
 		};
@@ -96,7 +96,7 @@ function onPart(source, args) {
 
 //hook for speaking
 function onMessage(source, text) {
-	lasthash[source.nick.toLowerCase()] = {
+	lastSeenHashStorage[source.nick.toLowerCase()] = {
 		'last': (new time.Date().getTime() / 1000),
 		'words': '"' + source.nick + '" last said "' + text + '" on "' + source.channel + '"'
 	};
@@ -110,31 +110,33 @@ function replyWithResult(source, args) {
 	var wantedNick = args[0].toLowerCase();
 	if (wantedNick === source.nick.toLowerCase()) {
 		source.mention('i see u right now...');
-	}else if(wantedNick === source.irc.currentNick){
+	} else if (wantedNick === source.irc.currentNick) {
 		source.mention('are you talking about me?');
-	} else if (typeof lasthash[wantedNick] !== 'undefined') {
-		source.reply(lasthash[wantedNick].words + ' ' + calcDiff(lasthash[wantedNick].last) + '.');
+	} else if (typeof lastSeenHashStorage[wantedNick] !== 'undefined') {
+		source.reply(lastSeenHashStorage[wantedNick].words + ' ' + calcDiff(lastSeenHashStorage[wantedNick].last) + '.');
 	} else {
 		source.mention('i don\'t know anything about "' + args[0] + '"');
 	}
 }
 
 exports.init = function () {
-    var savedhash = null;
-    try {
-        savedhash = JSON.parse(require('fs').readFileSync(BOT_DIR + '/lastseen.json'));
-    } catch(e) {
-        logger.warn('Lastseen file load failed, using empty file.');
-    }
-    lasthash = savedhash || {};
+	try {
+		lastSeenHashStorage = JSON.parse(require('fs').readFileSync(MODULES_DIR + '/lastseen/hash.json'));
+	} catch (e) {
+		logger.warn('Lastseen file load failed, using empty file.');
+		lastSeenHashStorage = {};
+	}
+	if (typeof lastSeenHashStorage !== 'object') {
+		lastSeenHashStorage = {};
+	}
 	this.addCommand('lastseen', replyWithResult).addCommand('seen', replyWithResult);
 	this.dispatcher.on('irc/QUIT', onQuit).on('irc/PART', onPart).on('irc/JOIN', onJoin).on('irc/NICK', onNick).on('irc/PRIVMSG', onMessage).on('irc/NOTICE', onMessage);
 };
 
 exports.halt = function () {
-    try {
-        require('fs').writeFileSync(BOT_DIR + '/lastseen.json', JSON.stringify(lasthash, null, 4));
-    } catch(e) {
-        //just ignore it
-    }
+	try {
+		require('fs').writeFileSync(MODULES_DIR + '/lastseen/hash.json', JSON.stringify(lastSeenHashStorage, null, 4));
+	} catch (e) {
+		logger.warn('Lastseen file save failed, i\'m now forgetting all the bronies i saw.');
+	}
 };
